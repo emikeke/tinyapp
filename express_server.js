f9823e533ada2294fac872c7bcee611095a02f14
Emily K <emily.ke@hotmail.com>
2021-09-27 13:03:38 -0600

    update

--- express_server.js
+++ express_server.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/urls', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
